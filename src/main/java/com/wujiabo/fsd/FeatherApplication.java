8db1607f16968d92e143cc2f82522b05d0466001
Jiabo.Wu <wujbow@cn.ibm.com>
2019-08-15 19:45:33 +0800

    init

---
+++ src/main/java/com/wujiabo/fsd/FeatherApplication.java
@@ -0,0 +1,13 @@
+package com.wujiabo.fsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatherApplication.class, args);
+	}
+
+}
